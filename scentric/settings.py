83c91b9ff7d86c8b3717c1548a77a22417f538d8
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2024-01-26 14:59:09 +0000

    update code for deploy

--- scentric/settings.py
+++ scentric/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7b3$yx=_yh&6!64+*46_@*=_+_=&ah*pcnoduznm8@rqdub%p0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-aleksandarjavo-scentric-zes09bh18k7.ws-eu107.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-aleksandarjavo-scentric-zes09bh18k7.ws-eu107.gitpod.io','.herokuapp.com']
 
 
 # Application definition
