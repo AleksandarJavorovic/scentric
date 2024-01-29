28b1f82f5ae3e356060fbdf8714ff3412e5b5604
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2024-01-29 16:09:14 +0000

    add active class to the header

--- scentric/urls.py
+++ scentric/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('blog.urls'), name='blog-urls'),
+    path('', include('blog.urls')),
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
 ]
