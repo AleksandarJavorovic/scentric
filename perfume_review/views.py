4f4945841b909f5e903f0ace9d3228e9d2e39088
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2024-02-01 14:34:56 +0000

    add view for perfume review posting

--- perfume_review/views.py
+++ perfume_review/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Perfume
 
-# Create your views here.
+
+class AddReview(CreateView):
+    '''
+    Add perfume review
+    '''
+    template_name = 'perfume_review/add_review.html'
+    model = Perfume
+    succes_url = '/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(addReview, self).form_valid(form)
