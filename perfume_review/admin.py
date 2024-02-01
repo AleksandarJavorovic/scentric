48a315e5895002bce29bb74a56fe5036f999b510
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2024-02-01 11:22:37 +0000

    update admin.py file of perfume_review app

--- perfume_review/admin.py
+++ perfume_review/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Perfume
 
-# Register your models here.
+
+@admin.register(Perfume)
+class PerfumeAdmin(admin.ModelAdmin):
+    list_display = (
+        "perfume_brand",
+        "perfume_name",
+        "perfume_group",
+        "top_notes",
+        "middle_notes",
+        "base_notes",
+        "image",
+        "description",
+        "base_notes",
+    )
+    list_filter = ("perfume_group",)
