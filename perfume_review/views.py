c16e718608d2b63a1d4362504e8265dc04a86414
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2024-02-02 21:22:52 +0000

    update AddReview view

--- perfume_review/views.py
+++ perfume_review/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import CreateView
 from .models import Perfume
-
+from .forms import PerfumeForm
 
 class AddReview(CreateView):
     '''
@@ -9,6 +9,7 @@ class AddReview(CreateView):
     template_name = 'perfume_review/add_review.html'
     model = Perfume
     succes_url = '/'
+    form_class = PerfumeForm
 
     def form_valid(self, form):
         form.instance.user = self.request.user
