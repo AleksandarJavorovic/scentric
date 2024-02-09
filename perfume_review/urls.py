48ac1f52fbe875fea1a68ea5ae91779002565c1e
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2024-02-09 21:52:19 +0000

    add url for edit_perfume view

--- perfume_review/urls.py
+++ perfume_review/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import AddReview, Perfumes, PerfumeDetail, DeletePerfume
+from .views import (
+    AddReview, Perfumes,
+    PerfumeDetail, DeletePerfume,
+    EditRecipe
+)
 
 
 urlpatterns = [
@@ -7,4 +11,5 @@ urlpatterns = [
     path("perfumes/", Perfumes.as_view(), name="perfumes"),
     path("<slug:pk>/", PerfumeDetail.as_view(), name="perfume_detail"),
     path("delete/<slug:pk>/", DeletePerfume.as_view(), name="delete_perfume"),
+    path("edit/<slug:pk>/", EditPerfume.as_view(), name="edit_perfume"),
 ]
