ca620ec47f0acc9e1ae92696b94161edaabc6ef3
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2024-02-09 21:29:37 +0000

    update perfume_confirm_delete template

--- perfume_review/urls.py
+++ perfume_review/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AddReview, Perfumes, PerfumeDetail
+from .views import AddReview, Perfumes, PerfumeDetail, DeletePerfume
 
 
 urlpatterns = [
     path("add_review/", AddReview.as_view(), name="add_review"),
     path("perfumes/", Perfumes.as_view(), name="perfumes"),
     path("<slug:pk>/", PerfumeDetail.as_view(), name="perfume_detail"),
+    path("delete/<slug:pk>/", DeletePerfume.as_view(), name="delete_perfume"),
 ]
