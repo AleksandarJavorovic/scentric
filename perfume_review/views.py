9c50cf98faeef9c9cd89ecd53bfd16dbee2ff3d1
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2024-02-09 20:54:26 +0000

    add template to delete perfume and 403 page

--- perfume_review/views.py
+++ perfume_review/views.py
@@ -1,6 +1,14 @@
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import (
+    CreateView, ListView,
+    DetailView, DeleteView
+)
+
 from .models import Perfume
 from .forms import PerfumeForm
+
+from django.contrib.auth.mixins import (
+    UserPassesTestMixin, LoginRequiredMixin
+)
 
 
 class PerfumeDetail(DetailView):
@@ -33,3 +41,14 @@ class AddReview(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(AddReview, self).form_valid(form)
+
+
+class DeletePerfume(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    '''
+    View to delete a perfume review
+    '''
+    model = Perfume
+    success_url = '/perfumes/'
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
