7f2174849b2cc2fd6f4ebb7780929bd4f917d22b
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2024-02-15 07:27:23 +0000

    update the secret key

--- scentric/settings.py
+++ scentric/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7b3$yx=_yh&6!64+*46_@*=_+_=&ah*pcnoduznm8@rqdub%p0'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
